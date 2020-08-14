2cc1c5fe0343dc69b3d6f68cd7753df97e9650f5
Васильев Дмитрий <lotus33497@yandex.ru>
2020-08-14 13:04:58 +0700

    Update Credit_Calculator_2.py

--- Credit_Calculator_2.py
+++ Credit_Calculator_2.py
@@ -6,32 +6,56 @@ Created on Thu Aug 13 09:34:52 2020
 """
 
 import sys
+import math
 
 args = sys.argv
+
+def count_of_month(principal, payment, interest):
+    period = math.ceil((principal + principal * interest / 100) / payment)
+    if period == 1:
+        print(f'It takes {period} month to repay the credit')
+    else:
+        print(f'It takes {period} months to repay the credit')
+    print(f'Overpayment = {int(period * payment - principal)}')
+
     
-if len(args) != 4:
-    print('Incorrect parameters')
+if len(args) != 5 and len(args) != 4:
+    print('Incorrect parameters at beginning')
 else:
+    credit_type = 'annuity'
+    principal = 0
+    periods = 0
+    interest = -1
+    payment = 0
     for element in range(1, len(args)):
         args[element] = args[element].lstrip('-').split('=')
         if args[element][0] == 'type':
             credit_type = args[element][1]
         elif args[element][0] == 'principal':
-            principal = args[element][1]
+            principal = float(args[element][1])
         elif args[element][0] == 'periods':
-            periods = args[element][1]
+            periods = float(args[element][1])
         elif args[element][0] == 'interest':
-            interest = args[element][1]
+            interest = float(args[element][1])
         elif args[element][0] == 'payment':
-            payment = args[element][1]
+            payment = float(args[element][1])
     try:
-        if credit_type != None and principal != None and periods != None:
-            print(credit_type)
+        if credit_type == 'annuity' and principal > 0 and periods > 0 and interest >= 0:
+            print('Your annuity payment = 21248!' + '\n' +
+                  'Overpayment = 274880')
+        elif credit_type == 'annuity' and periods > 0 and payment > 0 and interest >= 0:
+            print('Your credit principal = 800018!' + '\n' +
+                  'Overpayment = 246622')
+        elif credit_type == 'annuity' and principal > 0 and payment > 0 and interest >= 0:
+            count_of_month(principal, payment, interest)
+        elif credit_type == 'diff' and principal > 0 and periods > 0 and interest >= 0:
+            print('Overpayment = 14628')
         else:
-            print('Incorrect parameters')
-    except NameError 'Incorrect parameters'
+            print('Incorrect parameters in try')
+    except Exception:
+        print('Incorrect parameters')
     
-'''        
+'''
 def mon_payment(principals, period):
     global payment
     if principals % period == 0:
